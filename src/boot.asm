059b28792c8a79c0f01520bde2c623c7c3877751
kees <kees.lfs1k@gmail.com>
2023-10-05 04:56:33 +0700

    end up "hello world" task

--- src/boot.asm
+++ src/boot.asm
@@ -1,8 +1,42 @@
-ORG 0x7C00
-[BITS 16]
+ORG 0x7c00
+BITS 16
 
-loop:
-	jmp loop
+main:
+	mov ax, 0
+	mov ds, ax
+	mov es, ax
+	mov ss, ax
+
+	mov sp, 0x7c00
+	mov si, os_boot_msg
+	call print
+	hlt
+
+halt:
+	jmp halt
+
+print:
+	push si
+	push ax
+	push bx
+
+print_loop:
+	lodsb
+	or al, al
+	jz done_print
+	mov ah, 0x0e
+	mov bh, 0
+	int 0x10
+	jmp print_loop
+
+done_print:
+	pop bx
+	pop ax
+	pop si
+	ret
+
+os_boot_msg:
+	DB 'Hello world', 0x0d, 0x0a, 0
 
 times 510-($-$$) db 0
 
